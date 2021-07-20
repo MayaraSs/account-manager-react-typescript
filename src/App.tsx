e8a8d8dc755912376107fce09ea7dda20c6f7888
MayaraSs <stein.mayara@gmail.com>
2021-07-20 16:15:52 -0300

    edit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
      <h1> Test</h1>
+     <GlobalStyle/>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {App} from './App'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +9,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start meWasuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
