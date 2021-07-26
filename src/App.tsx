e3467ac15f6920ddf0319516e8496385562d7fe4
MayaraSs <stein.mayara@gmail.com>
2021-07-26 15:33:08 -0300

    Add props in Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard';
+import Modal from 'react-modal'
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+ 
   return (
     <>
-     <Header/>
+     <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
      <Dashboard/>
+     <Modal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}>
+        <h2>Cadastrar</h2>
+    </Modal>
      <GlobalStyle/>
     </>
   );
